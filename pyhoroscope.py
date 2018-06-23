08bc843ae1dd0a3ca6db527932bf081c18ce3659
Tapasweni Pathak <tapasweni@mapbox.com>
2018-06-23 18:47:46 +0530

    Add midnight time calculation
    ref https://github.com/tapasweni-pathak/Horoscope-API/issues/36

--- pyhoroscope.py
+++ pyhoroscope.py
@@ -1,6 +1,6 @@
 from lxml import html
 import requests
-import re
+from datetime import datetime, timezone
 
 ####################################################################
 # API
@@ -10,18 +10,35 @@ class Horoscope:
 
     @staticmethod
     def get_todays_horoscope(sunsign):
-        print("hi")
         url = "http://www.ganeshaspeaks.com/horoscopes/daily-horoscope/" + sunsign
         response = requests.get(url)
         tree = html.fromstring(response.content)
         date = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
         date = date.replace("']", "").replace("['", "")
-        horoscope = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
-        horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("[\"", "").replace("\"]", "")
+        date_utc = datetime.now(timezone.utc)
+        date_website = "-".join(date.split('-')[::-1])
+        date_local = str(date_utc.astimezone()).split(' ')[0]
+
+        if date_local < date_website :
+            url = "https://www.ganeshaspeaks.com/horoscopes/yesterday-horoscope/" + sunsign
+            response = requests.get(url)
+            tree = html.fromstring(response.content)
+            horoscope = str(tree.xpath(
+                "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+        elif date_local > date_website :
+            url = "https://www.ganeshaspeaks.com/horoscopes/tomorrow-horoscope/" + sunsign
+            response = requests.get(url)
+            tree = html.fromstring(response.content)
+            horoscope = str(tree.xpath(
+                "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+        else :
+            horoscope = str(tree.xpath(
+            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+
+        horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("[\"", "").replace("\"]", "").replace("[\'", "").replace("\']", "")
         dict = {
-            'date': date,
+            'date': date_local,
             'horoscope': horoscope,
             'sunsign': sunsign
         }
@@ -34,10 +51,10 @@ class Horoscope:
         response = requests.get(url)
         tree = html.fromstring(response.content)
         week = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
         week = week.replace("']", "").replace("['", "")
         horoscope = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
         horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
         dict = {
             'week': week,
@@ -53,10 +70,10 @@ class Horoscope:
         response = requests.get(url)
         tree = html.fromstring(response.content)
         month = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
         month = month.replace("']", "").replace("['", "")
         horoscope = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()[1]"))
+            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()[1]"))
         horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
         dict = {
             'month': month,
@@ -72,10 +89,10 @@ class Horoscope:
         response = requests.get(url)
         tree = html.fromstring(response.content)
         year = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
         year = year.replace("']", "").replace("['", "")
         horoscope = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
         horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
         dict = {
             'year': year,
