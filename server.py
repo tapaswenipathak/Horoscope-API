69ef1d1306e030ecae17ce92efc904c85551921a
Tapasweni Pathak <tapaswenipathak@gmail.com>
2019-01-27 05:41:35 +0530

    Add CORS extension
    Closes https://github.com/tapaswenipathak/Horoscope-API/issues/34.

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from pyhoroscope import Horoscope
+from flask_cors import CORS
 
 app = Flask (__name__)
+CORS(app)
 
 ############################################
 # Index 
