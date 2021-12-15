40ae235ea89178e8c741e797a322773d9708ee59
samloik <92296590+samloik@users.noreply.github.com>
2021-12-15 11:25:58 +1000

    Add files via upload
    Changed the parsing method to beautifulsoup.

--- pyhoroscope.py
+++ pyhoroscope.py
@@ -1,6 +1,7 @@
 from lxml import html
 import requests
 from datetime import datetime, timezone
+from bs4 import BeautifulSoup
 
 ####################################################################
 # API
@@ -12,10 +13,9 @@ class Horoscope:
     def get_todays_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/horoscopes/daily-horoscope/" + sunsign
         response = requests.get(url)
-        tree = html.fromstring(response.content)
-        date = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        date = date.replace("']", "").replace("['", "")
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        date = soup.find('p', class_='mb-0').text
         date_utc = datetime.now(timezone.utc)
         date_website = "-".join(date.split('-')[::-1])
         date_local = str(date_utc.astimezone()).split(' ')[0]
@@ -23,18 +23,17 @@ class Horoscope:
         if date_local < date_website :
             url = "https://www.ganeshaspeaks.com/horoscopes/yesterday-horoscope/" + sunsign
             response = requests.get(url)
-            tree = html.fromstring(response.content)
-            horoscope = str(tree.xpath(
-                "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+            soup = BeautifulSoup(response.text, 'lxml')
+            horoscope = soup.find('p', id='horo_content').text
+
         elif date_local > date_website :
             url = "https://www.ganeshaspeaks.com/horoscopes/tomorrow-horoscope/" + sunsign
             response = requests.get(url)
-            tree = html.fromstring(response.content)
-            horoscope = str(tree.xpath(
-                "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+            soup = BeautifulSoup(response.text, 'lxml')
+            horoscope = soup.find('p', id='horo_content').text
+
         else :
-            horoscope = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+            horoscope = soup.find('p', id='horo_content').text
 
         horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("[\"", "").replace("\"]", "").replace("[\'", "").replace("\']", "")
         dict = {
@@ -49,12 +48,9 @@ class Horoscope:
     def get_weekly_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/horoscopes/weekly-horoscope/" + sunsign
         response = requests.get(url)
-        tree = html.fromstring(response.content)
-        week = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        week = week.replace("']", "").replace("['", "")
-        horoscope = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+        soup = BeautifulSoup(response.text, 'lxml')
+        week = soup.find('p', class_='mb-0').text
+        horoscope = soup.find('p', id='horo_content').text
         horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
         dict = {
             'week': week,
@@ -68,12 +64,9 @@ class Horoscope:
     def get_monthly_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/horoscopes/monthly-horoscope/" + sunsign
         response = requests.get(url)
-        tree = html.fromstring(response.content)
-        month = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        month = month.replace("']", "").replace("['", "")
-        horoscope = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()[1]"))
+        soup = BeautifulSoup(response.text, 'lxml')
+        month = soup.find('p', class_='mb-0').text
+        horoscope = soup.find('p', id='horo_content').text
         horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
         dict = {
             'month': month,
@@ -87,12 +80,9 @@ class Horoscope:
     def get_yearly_horoscope(sunsign):
         url = "http://www.ganeshaspeaks.com/horoscopes/yearly-horoscope/" + sunsign
         response = requests.get(url)
-        tree = html.fromstring(response.content)
-        year = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        year = year.replace("']", "").replace("['", "")
-        horoscope = str(tree.xpath(
-            "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+        soup = BeautifulSoup(response.text, 'lxml')
+        year = soup.find('p', class_='mb-0').text
+        horoscope = soup.find('p', id='horo_content').text
         horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
         dict = {
             'year': year,
@@ -101,3 +91,15 @@ class Horoscope:
         }
 
         return dict
+
+
+sunsigns = ["aries", "taurus", "gemini", "cancer", "leo", "virgo",
+           "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
+timeframe = ["todays", "weekly", "yearly", "monthly"]
+
+
+print(sunsigns[0])
+print(Horoscope.get_todays_horoscope(sunsigns[0]))
+print(Horoscope.get_weekly_horoscope(sunsigns[0]))
+print(Horoscope.get_monthly_horoscope(sunsigns[0]))
+print(Horoscope.get_yearly_horoscope(sunsigns[0]))
